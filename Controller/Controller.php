cbeb1c6cea3b35abf299d80230137a9ee586cf1b
Ricardo Felisbino <ricardo.relativo@gmail.com>
2018-02-04 08:48:29 -0200

    inclusão função listar
    realiza conexão com base de dados, e lista todas as tarefas existentes no sistema

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -115,15 +115,98 @@ class Controller {
     }	
 	
     public function listar() {
-	
+		
+        /* conecta ao MySQL pelo Model e realiza a consulta das tarefas */
+        $model = new Model();
+        $consulta = "SELECT * FROM tarefas"; 
+        $sql = mysqli_query(($model->conectar()), $consulta);
+        
+        echo '<div class="div-table">
+                <div class="div-table-row div-table-head">
+                    <div class="div-table-cell">#</div>
+                    <div class="div-table-cell">Dia</div>
+                    <div class="div-table-cell">Horário</div>
+                    <div class="div-table-cell">Descrição</div>
+                    <div class="div-table-cell">Editar</div>
+                    <div class="div-table-cell">Excluir</div>
+                </div>';
+        
+        /* verifica se há regisatros no sistyema */
+        if(mysqli_num_rows($sql)>0){
+            
+            /* inicio do loop */    
+            while($linhas = mysqli_fetch_assoc($sql)):
+            ?>
+                                <div class="div-table-row">
+                                <div class="div-table-cell"><?= $linhas['id'] ?></div>
+                                <div class="div-table-cell"><?= $linhas['data'] ?></div>
+                                <div class="div-table-cell"><?= $linhas['horario'] ?></div>
+                                <div class="div-table-cell"><?= $linhas['nome'] ?></div>
+                                <div class="div-table-cell">
+                                <input TYPE="BUTTON" NAME="submit" class="bt_ListarEditar" value="Editar" onclick="alterar('<?php echo $linhas["id"] . "', '" . $linhas["data"] . "', '" . $linhas["horario"] . "', '" . $linhas["nome"]; ?>')" >
+                                </div>
+                                <div class="div-table-cell">
+                                <input TYPE="BUTTON" NAME="submit" class="bt_ListarExcluir" value="Excluir" onclick="apagar('<?= $linhas['id'] ?>')" >
+                                </div>
+
+                                </div>
+
+            <?php
+            /* termino do loop */       
+            endwhile;   
+                        
+        } else {
+            /* caso não tenha registro no banco de dados */
+                    echo '
+                <div class="div-table">
+                    <div class="div-table-row">
+                        <div class="div-table-cell">Não há registrados no sistema
+                        </div>
+                    </div>
+                </div>';
+        }
+
+        
 	}
 	
     public function editar($id,$data,$horario,$nome) {
-   
+       
+	
+		echo '  
+		<div class="div-table">
+            <div class="div-table-row">
+                <div class="div-table-cell"><h3>Tarefa selecionada: <b>#' . $id . '</b></h3></div>
+            </div><div class="div-table-row">
+                <div class="div-table-cell" >
+                <label>Dia:</label>
+                <input type="date" name="data" value="'. $data .'"></div>
+            </div><div class="div-table-row">
+                <div class="div-table-cell" >
+                <label>Horário:</label>
+                <input type="time" name="horario" value="'. $horario .'"></div>
+            </div><div class="div-table-row">
+                <div class="div-table-cell" >
+                <label>Descrição:</label>
+                <input type="text" name="nome" value="'. $nome .'"></div>
+            </div><div class="div-table-row">
+                <div class="div-table-cell" >
+                <button type="submit" class="bt_SalvaTarefa">Salvar</button>
+                </div>
+            </div>
+        </div>';
+        
     }
     
     public function excluir($id) {
-
+    		
+        echo '
+        <div class="div-table">
+            <div class="div-table-row">
+                <div class="div-table-cell">
+                <h3>Tarefa (' . $id . ') será excluida em breve</h3>
+                </div>
+            </div>
+        </div>';
     }
     
 }
