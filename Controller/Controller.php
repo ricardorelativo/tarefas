c8cec34ccb5431d74d80d9adc46d687fac09cd21
Ricardo Felisbino <ricardo.relativo@gmail.com>
2018-02-03 23:38:06 -0200

    inclusão menu
    criação das funções para exibir menu do sistema

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -2,7 +2,73 @@
 
 class Controller {
     
-       
+    /* função para criar os botões de navegação */
+    public function botoes($tipo) {
+        
+        /* personalizando o msg sistema  */
+        switch($tipo)   {
+    
+            case "tarefa":
+                    $msg = ' - Listar';
+            break;
+            case "nova":
+                    $msg = ' - Nova';
+            break;
+            default: 
+
+                    $msg = ' - Home';
+
+            break;
+        }
+        
+        /* exibe lista dos botões */
+        echo '
+         <div class="div-table">
+            <div class="div-table-row">
+                <div class="div-table-cell"><h3>Lista de Tarefas'. $msg .'</h3>
+                </div>
+            </div>
+        </div>
+                <div id="bt_Geral" >Página Inicial</div>
+                <div id="bt_Listar" >Listar Tarefas</div>
+                <div id="bt_Adicionar" >Adicionar Tarefa</div>
+        ';
+        
+    }
+	
+	public function navegacao($operacao) {
+        
+        switch($operacao)   {
+    
+            case "Listar":
+
+            $view = new View();
+            $view->opcao('tarefa'); 
+            $view->listar(); 
+
+            break;
+
+            case "Adicionar":
+
+            $view = new View();
+            $view->opcao('nova'); 
+            $view->adicionar(); 
+
+            break;  
+
+            default: 
+
+            $view = new View();
+            $view->opcao('geral'); 
+            $view->home(); 
+
+            break;
+                
+        }
+           
+        
+    }
+	
     public function adicionar() {
       
     }	
--- View/View.php
+++ View/View.php
@@ -6,14 +6,31 @@ class View {
     public function home() {
         
         echo '
-        
-        <h3>Sistema está no momento em desenvolvimento</h3>
-		<p>Desenvolvido por <b>Ricardo Felisbino</b></p>
-		
+        <div class="div-table">
+			<div class="div-table-row">
+				<div class="div-table-cell">
+				<h3>Sistema está no momento em desenvolvimento</h3>
+				</div>
+			</div>
+			<div class="div-table-row">
+				<div class="div-table-cell">
+				<p>Desenvolvido por <b>Ricardo Felisbino</b></p>
+				</div>
+			</div>
+        </div>
 		';
         
     }
     
+	/* função para exibir opções do menu */
+    public function opcao($tipo) {
+	
+       /* consulta as operações do Controller */
+       $controller = new Controller();
+       $controller->botoes($tipo); 
+        
+        
+    }
     
     public function listar() {
         
@@ -27,6 +44,10 @@ class View {
 	}
     
     
+    public function salvar() {
+		
+		
+	}
 }
 
 ?>
