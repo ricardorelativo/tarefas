44bf334eedd8946c38076671426cb70877018bd3
Ricardo Felisbino <ricardo.relativo@gmail.com>
2018-02-05 10:32:04 -0200

    Atualização dados SQL
    alteração das informações da tabela SQL

--- vistasoft.sql
+++ vistasoft.sql
@@ -15,16 +15,17 @@ CREATE TABLE IF NOT EXISTS `tarefas` (
   `horario` time NOT NULL,
   `nome` varchar(255) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `tarefas`
 --
 
 INSERT INTO `tarefas` (`id`, `data`, `horario`, `nome`) VALUES
-(3, '2018-02-05', '11:00:00', 'preparar almoço'),
-(4, '2018-02-05', '12:00:00', 'comer almoço'),
-(5, '2018-02-05', '13:00:00', 'limpar cozinha'),
-(6, '2018-02-05', '15:00:00', 'entrevista de emprego');
+(1, '2018-02-02', '17:00:00', 'Inicio do desenvolvimento do Gerenciado de Tarefas'),
+(3, '2018-02-05', '10:30:00', 'Realizar verificação do código fonte'),
+(8, '2018-02-05', '15:00:00', 'Entrevista de emprego na VistaSoft'),
+(9, '2018-02-05', '17:00:00', 'Contrato de trabalho com a VistaSoft'),
+(10, '2019-01-12', '19:00:00', 'Churrasco no apartamento do Ricardo');
 COMMIT;
 
